5c6a04721ba7a9ddd4ce04aea7465a1fccf44334
Celin42 <78828149+Celin42@users.noreply.github.com>
2021-05-11 18:57:38 -0300

    Update Pessoas.h

--- Pessoas.h
+++ Pessoas.h
@@ -1,21 +1,22 @@
 #ifndef PESSOAS_H
 #define PESSOAS_H
+#define QUANTIDADE_DE_PESSOAS 1000
 #include <string>
 
 class Pessoas{
     protected:
-        std::string _nome;
-        std::string _cpf;
-        std::string _telefone;
-        char _genero;
+        std::string *_nome;
+        std::string *_cpf;
+        std::string *_telefone;
+        char *_genero;
 
     public:
         Pessoas(std::string nome, char genero, std::string cpf, std::string telefone);
-        virtual void exibirDados() = 0;
-        virtual std::string get_nome() = 0;
-        virtual std::string get_cpf() = 0;
-        virtual std::string get_telefone() = 0;
-        virtual char get_genero() = 0;
+        void exibirDados() = 0;
+        std::string get_nome();
+        std::string get_cpf();
+        std::string get_telefone();
+        char get_genero();
         ~Pessoas();
 };
 
