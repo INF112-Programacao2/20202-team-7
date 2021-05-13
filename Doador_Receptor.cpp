1d8d2841ece55d7805774b33ccb213d555c765d0
Vitor Emanuel Pereira Silva <vitor.emanuel@ufv.br>
2021-05-13 18:00:08 -0300

    alterações em pessoas

--- Doador_Receptor.cpp
+++ Doador_Receptor.cpp
@@ -1,15 +1,61 @@
 #include "Doador_Receptor.h"
+#include "Pessoas.h"
+#include "Paciente.h"
 #include <iostream>
 #include <fstream>
+#include<vector>
 
 
 Doador_Receptor::Doador_Receptor(std::string nome, char genero, std::string cpf, std::string telefone, std::string planosaude,std::string tipo_sanguineo,int quantidade_de_sangue):
-    Pessoas(nome,genero,cpf,telefone), Paciente(planosaude), tipo_sanguineo(tipo_sanguineo), quantidade_de_sangue(quantidade_de_sangue) {}
-
+    Pessoas(nome,genero,cpf,telefone),Paciente(planosaude), _tipo_sanguineo(tipo_sanguineo), _quantidade_de_sangue(quantidade_de_sangue) {}
 void Doador_Receptor::Adicionar_sangue(){
+
+    std::ifstream arquivo_saida;
+    arquivo_saida.open("tipagem.txt");
+
+    if (!arquivo_saida.is_open())
+    {
+        throw "Arquivo inexistente";
+    }
+
+    std::vector<std::string> quant_sangue;
+    char virgula = ',';
+    int contador = 0;
+    std::string palavra;
+
+    while (!arquivo_saida.eof())
+    {
+        std::getline(arquivo_saida,palavra,virgula);
+        if(contador%2 == 0){
+            quant_sangue.push_back(palavra);
+        }
+        contador++;
+    }
+
+
+    
+
+
+    std::ofstream gravar_informacoes;
+
+    gravar_informacoes.open("doadores.txt");
+
+    if (!gravar_informacoes.is_open())
+    {
+        throw "Arquivo inexistente";
+    }
+
 
 }    
 
 void Doador_Receptor::Retirar_sangue(){
 
 } 
+
+int Doador_Receptor::get_quantidade_de_sangue(){
+    return this->_quantidade_de_sangue;
+}
+
+std::string Doador_Receptor::get_tipo_sanguineo(){
+    return this-> _tipo_sanguineo;
+}
--- Pessoas.cpp
+++ Pessoas.cpp
@@ -2,39 +2,22 @@
 #include <string>
 
 
-Pessoas::Pessoas(std::string *nome, char *genero, std::string *cpf, std::string *telefone){
-    _nome = new std::string[QUANTIDADE_DE_PESSOAS];
-    _genero = new char[QUANTIDADE_DE_PESSOAS];
-    _cpf = new std::string[QUANTIDADE_DE_PESSOAS];
-    _telefone = new std::string[QUANTIDADE_DE_PESSOAS];
+Pessoas::Pessoas(std::string nome, char genero, std::string cpf, std::string telefone):
+    _nome(nome), _genero (genero), _cpf (cpf), _telefone(telefone) {}
 
-    for(int i = 0; i < QUANTIDADE_DE_PESSOAS; i++){
-        _nome[i] = nome[i];
-        _genero[i] = genero[i];
-        _cpf[i] = cpf[i];
-        _telefone[i] = telefone[i];
-    }
+
+std::string Pessoas::get_nome(){
+    return this->_nome;
 }
 
-std::string Pessoas::get_nome(int n){
-    return this->_nome[n];
+std::string Pessoas::get_cpf(){
+    return this->_cpf;
 }
 
-std::string Pessoas::get_cpf(int n){
-    return this->_cpf[n];
+std::string Pessoas::get_telefone(){
+    return this->_telefone;
 }
 
-std::string Pessoas::get_telefone(int n){
-    return this->_telefone[n];
-}
-
-char Pessoas::get_genero(int n){
-    return this->_genero[n];
-}
-
-Pessoas::~Pessoas(){
-    delete[] _nome;
-    delete[] _genero;
-    delete[] _cpf;
-    delete[] _telefone;
+char Pessoas::get_genero(){
+    return this->_genero;
 }
