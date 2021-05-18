b54e521c6cb7d540c46e15eb0515eba55ee0e38f
Belle <isabelle.matos@ufv.br>
2021-05-17 21:18:52 -0300

    Banco de sangue comentado

--- Banco_de_Sangue.h
+++ Banco_de_Sangue.h
@@ -1,11 +1,20 @@
 #ifndef BANCO_DE_SANGUE_H
 #define BANCO_DE_SANGUE_H
 
+    /* Classe responável para emitir os dados sobre o banco de sangue do hospital, como
+    quantidade de sangue total, quantidade de cada tipo e as informações sobre os
+    doadores e receptores
+    */
 class Banco_de_Sangue
 {
 public:
+    // Método que informa os dados dos doadores e receptores de sangue 
     void DadosBancoSangue();
+
+    // Método que informa a quantidade total de sangue do hospital
     void QuantidadeTotal();
+
+    //Método que informa a quantidade de cada tipo de sangue do hospital (A,AB,B ou O)
     void QuantidadeTipo();
     
     
--- Banco_de_sangue.cpp
+++ Banco_de_sangue.cpp
@@ -6,21 +6,23 @@
 
 void Banco_de_Sangue::DadosBancoSangue()
 {
-
+    // São criadas variáveis que irão ser relacionadas a cada tipo de informação presente no arquivo
     std::string sexo_d, nome, tipo_recptor, tipo_doador, cpf_d, tel_d, plsaude_d, sexo_r, _nome, cpf_r, tel_r, plsaude_r;
+    // Classes de fluxos de arquivos que irão operar o txt
     std::ifstream doa, rec;
     int qtd_doada, qtd_recebida;
 
+    // É feita a abertura do arquivo que contém os dados dos doadores
     doa.open("doadores.txt");
 
-   
+   //Exceção caso o arquivo não exista
     if (!doa.is_open())
     {
         throw "Arquivo inexistente.";
-
     }
     std::cout << "\t\t" << std::right << "SEXO:" << std::setw(11) << "DOADORES:" << std::setw(15) << "CPF:" << std::setw(17) << "TELEFONE:" << std::setw(12) << "CONVENIO:" << std::setw(8) << "TIPO:" << std::setw(18) << "QUANTIDADE/mL:" << std::endl;
 
+    // É feita a leitura do arquivo, inicializando cada variável criada com os valores presentes nos arquivos
     while (!doa.eof())
     {
         std::getline(doa, sexo_d, ',');
@@ -38,19 +40,20 @@ void Banco_de_Sangue::DadosBancoSangue()
         doa >> qtd_doada;
         doa.ignore();
 
+        // É feita a impressão dos dados. Utilizou-se os manipuladores std::left e std::setw() para configurar a formatação da saída
         std::cout << "\t\t" << std::left << std::setw(7) << sexo_d <<std::setw(20) << nome << std::setw(12) << cpf_d << std::setw(12) << tel_d << std::setw(12) << plsaude_d << std::setw(9) << tipo_doador << qtd_doada << std::endl;
         doa.ignore();
     }
     std::cout << std::endl;
     doa.close();
 
+    // É feita a abertura do arquivo que contém os dados de receptores
     rec.open("receptores.txt");
 
-  
+    // Exceção caso o arquivo não exista
     if (!rec.is_open())
     {
-        throw "Arquivo inexistente.";
-        
+        throw "Arquivo inexistente.";        
     }
 
     std::cout << "\t\t" << std::right << "SEXO:" << std::setw(13) << "RECEPTORES:" << std::setw(13) << "CPF:" << std::setw(17) << "TELEFONE:" << std::setw(12) << "CONVENIO:" << std::setw(8) << "TIPO:" << std::setw(18) << "QUANTIDADE/mL:" << std::endl;
@@ -82,29 +85,36 @@ void Banco_de_Sangue::DadosBancoSangue()
 
 void Banco_de_Sangue::QuantidadeTotal()
 {
-
+    // Classe de fluxo de arquivo que irá operar o txt
     std::ifstream tip;
     std::string tipo;
+
+    // São criadas variáveis que irão ser relacionadas a cada tipo de informação presente no arquivo
     int i = 0, qtd_sangue, qtd_total[i], qtd_final = 0;
+
+    // Abertura do arquivo
     tip.open("tipagem.txt");
 
-  
+    
+    // Exceção caso o arquivo não exista
     if (!tip.is_open())
     {
-        throw "Arquivo inexistente.";
-   
+        throw "Arquivo inexistente.";  
     }
 
+    // É feita a leitura do arquivo
     while (!tip.eof())
     {
         tip >> qtd_sangue;
         tip.ignore();
 
         std::getline(tip, tipo, ',');
+        // Armazena em um vetor a quantidade de sangue de cada tipo
         qtd_total[i] = qtd_sangue;
         i++;
     }
 
+    // É feito um loop para adicionar a quantidade total na variável qtd_final
     for (int j = 0; j < i - 1; j++)
     {
         qtd_final += qtd_total[j];
@@ -117,27 +127,34 @@ void Banco_de_Sangue::QuantidadeTotal()
 
 void Banco_de_Sangue::QuantidadeTipo()
 {
-
+    // Classe de fluxo de arquivo que irá operar o txt
     std::ifstream tip;
     std::string tipo;
     int i = 0, qtd;
+
+    // É feita a abertura do arquivo que contém os tipos e quantidades de sangue
     tip.open("tipagem.txt");
 
   
+    // Exceção caso o arquivo não exista
     if (!tip.is_open())
     {
         throw "Arquivo inexistente.";
     }
     std::cout << "\t\t" << std::right << "QUANTIDADE/mL:" << std::setw(10) << "TIPO:" << std::endl;
 
-    // 4 tipos de sangue
+    /* Como são 4 tipos de sangue, o loop irá ler o arquivo ate que o iterador
+        i seja diferente de 4
+    */
     while (i != 4)
     {
         tip >> qtd;
         tip.ignore();
 
+    
         std::getline(tip, tipo, ',');
-
+        
+        // Será impresso a quantidade de sangue e o tipo 
         std::cout << "\t\t" << std::left << std::setw(19) << qtd << tipo << std::endl;
         i++;
     }
