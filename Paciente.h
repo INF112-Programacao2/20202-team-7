65e1c3d61aede4f568ce938ca1c312f67ab11504
Patrick de Angeli <ptkangel11@gmail.com>
2021-05-18 09:17:08 -0300

    adição ao comentarios

--- Paciente.h
+++ Paciente.h
@@ -2,20 +2,24 @@
 #define PACIENTE_H
 #include "Pessoas.h"
 
+/* Criação da classe Paciente, responsavel pelas funções previamente definidas no CRC, tais quais o marcação de consulta, verificação de plano e regisstro do paciênte.
+*/
+
 class Paciente : public Pessoas
 {
 private:
     std::string _planosaude;
-    
+    /*Especificação dos atributos referentes unicamente à classe Paciente.*/
+
 public:
 
-   
+     /*Construtor que receberá dados da Classe pessoa e também da classe Paciente.*/
     Paciente(const char &genero, const std::string &_nome, const std::string &cpf, const std::string telefone, std::string planosaude);
-
-    void exibirDados();
-    void Marcar_Consulta();
-    void Cancelar_Consulta();
-    std::string get_planosaude();  
+    
+    void exibirDados(); // exibirá os dados referente ao paciente escolhido
+    void Marcar_Consulta(); // responsável por agendar a consulta de acordo com o horairo.
+    void Cancelar_Consulta(); // responsavel por desmarcar a consulta.
+    std::string get_planosaude();  //responsavel somente por armazanar o plano de saude.
    
 };
 
